94376b54ff57940e96e0570a76361b4484d8145b
yuhki <yuhki79sa@gmail.com>
2025-03-23 15:56:01 +0900

    四択アンケート修正完了

--- app/Http/Controllers/ChoiceController.php
+++ app/Http/Controllers/ChoiceController.php
@@ -13,7 +13,9 @@ class ChoiceController extends Controller
     
     public function choice(Post $post){
         
+         
          $choiceCategories = ChoiceCategory::all();
+        
         
         return view('news.choice')->with(['post'=>$post, 'choiceCategories'=>$choiceCategories]);
     }
@@ -23,20 +25,27 @@ class ChoiceController extends Controller
         $post_id = $request->post_id;
         $choiceCategory_id = $request->choiceCategory_id;
         
-        $alreadyChoiced = Choice::where('user_id', $user_id)->where('post_id', $post_id)->where('choiceCategory_id', $choiceCategory_id)->first();
+        $alreadyChoiced = Choice::where('user_id', $user_id)->where('post_id', $post_id)->first();
+       
+        
         
         if(!$alreadyChoiced){
-        
-        $choice = new Choice;
-        $choice->user_id = $user_id;
-        $choice->post_id = $post_id;
-        $choice->choiceCategory_id = $choiceCategory_id;
-        $choice->save();
+            $choice = new Choice;
+            $choice->post_id = $post_id;
+            $choice->user_id = $user_id;
+            $choice->choiceCategory_id = $choiceCategory_id;
+            $choice->save();
         }
-        
+            
         else{
-          Choice::where('user_id', $user_id)->where('post_id', $post_id)->where('choiceCategory_id', $choiceCategory_id)->delete();
+            Choice::where('user_id', $user_id)->where('post_id', $post_id)->delete();
+             $choice = new Choice;
+            $choice->post_id = $post_id;
+            $choice->user_id = $user_id;
+            $choice->choiceCategory_id = $choiceCategory_id;
+            $choice->save();
         }
+
         
         return redirect('/posts');
         
--- resources/views/news/choice.blade.php
+++ resources/views/news/choice.blade.php
@@ -6,10 +6,11 @@
     @csrf
     <input type="hidden" name="post_id" value="{{ $post->id }}">
     <select name="choiceCategory_id">
+        <option value="" disabled {{ old('choiceCategory_id') ? '' : 'selected'}}>選択してください</option>
     @foreach($choiceCategories as $choiceCategory)
-    <option value="{{ $choiceCategory->id }}">
-        {{ $choiceCategory->name }}
-        </option>
+    <option value="{{ $choiceCategory->id }}" {{ old('choiceCategory_id') == $choiceCategory->id ? 'selected' : '' }}>
+    {{ $choiceCategory->name }}
+    </option>
     @endforeach
     </select>
     <input type="submit" value="送信">
